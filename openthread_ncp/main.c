674181f0340dabf6df9dbc81c217f46701304178
Hyungsin <hskim@berkeley.edu>
2017-11-06 15:34:05 -0800

    openthread app update

--- openthread_ncp/main.c
+++ openthread_ncp/main.c
@@ -15,16 +15,8 @@
 
 #include <stdio.h>
 
-#include "ot.h"
-#ifndef SAMPLE_INTERVAL
-#define SAMPLE_INTERVAL (1000000UL)
-#endif
 int main(void)
 {
     /* Run wpantund to interact with NCP */
-    /*while(1) {
-        xtimer_usleep(SAMPLE_INTERVAL);
-        LED0_TOGGLE;
-    }*/
     return 0;
 }
