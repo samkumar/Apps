58fb7bb14217e7c758be6718988c5d2062e7bdaa
Hyungsin <hskim@berkeley.edu>
2018-01-18 15:24:05 -0800

    openthread_ncp: add network status variables

--- openthread_ncp/main.c
+++ openthread_ncp/main.c
@@ -15,6 +15,23 @@
 
 #include <stdio.h>
 
+uint8_t borderRouteChangeCnt = 0;
+uint8_t borderRouterLC = 0;
+uint8_t borderRouterRC = 0;
+uint16_t myRloc = 0;
+uint16_t nextHopRloc = 0;
+uint8_t debugNumFreeBuffers = 0;
+
+uint32_t addressMsgCnt = 0;
+uint32_t joiningMsgCnt = 0;
+uint32_t routingMsgCnt = 0;
+uint32_t linkMsgCnt = 0;
+uint32_t controlMsgCnt = 0;
+uint32_t packetSuccessCnt = 0;
+uint32_t packetFailCnt = 0;
+uint32_t packetBusyChannelCnt = 0;
+uint32_t broadcastCnt = 0;
+
 int main(void)
 {
     /* Run wpantund to interact with NCP */
